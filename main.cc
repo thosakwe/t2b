0547a460e9f23dfd63782589df3850906485ec20
Tobe O <thosakwe@gmail.com>
2018-06-11 20:09:49 -0400

    GPL

--- main.cc
+++ main.cc
@@ -3,7 +3,7 @@
 // All rights reserved.
 //
 // Use of this source code is governed by an
-// MIT-style license that can be found in the LICENSE file.
+// GPL license that can be found in the LICENSE file.
 #include <cstdint>
 #include <ctime>
 #include <map>
