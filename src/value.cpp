9a9a873a7dd5536577d9283d3d74699155735a22
Tobe O <thosakwe@gmail.com>
2019-02-25 10:08:29 -0500

    Move value class to separate file

--- src/value.cpp
+++ src/value.cpp
@@ -4,13 +4,4 @@
 //
 // Use of this source code is governed by a
 // license that can be found in the LICENSE file.
-#ifndef T2B_AST_HPP
-#define T2B_AST_HPP
-
-namespace t2b
-{
-
-}
-
-
-#endif //T2B_AST_HPP
+#include <t2b/value.hpp>
