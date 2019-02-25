7742fd630446edec7515c1f4c7c8b889ba82ec7f
Tobe O <thosakwe@gmail.com>
2019-02-25 10:22:55 -0500

    Add ast classes

--- include/t2b/ast.hpp
+++ include/t2b/ast.hpp
@@ -7,9 +7,62 @@
 #ifndef T2B_AST_HPP
 #define T2B_AST_HPP
 
+#include <string>
+#include <vector>
+#include <t2b/value.hpp>
+
 namespace t2b
 {
+    class t2b_visitor;
 
+    class value_ctx
+    {
+
+    };
+
+    struct command_call_ctx : public value_ctx
+    {
+        std::string command_name;
+        std::vector<value_ctx> arguments;
+    };
+
+    struct constant_value_ctx : public value_ctx
+    {
+        value constant;
+    };
+
+    class stmt_ctx
+    {
+    public:
+        // TODO: Make this generic?
+        virtual void accept(t2b_visitor &visitor) = 0;
+    };
+
+    class macro_ctx
+    {
+        std::vector<std::string> params;
+        std::vector<stmt_ctx> body;
+    };
+
+    struct if_stmt_ctx : public stmt_ctx
+    {
+
+    };
+
+    struct manda_stmt_ctx : public stmt_ctx
+    {
+        macro_ctx macro;
+    };
+
+    struct times_stmt_ctx : public stmt_ctx
+    {
+        std::vector<stmt_ctx> body;
+    };
+
+    struct compilation_unit_ctx
+    {
+        std::vector<stmt_ctx> stmts;
+    };
 }
 
 
