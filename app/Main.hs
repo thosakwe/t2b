c169b1144c49ea58e2e40294b46ef5f1bb931222
Tobe O <thosakwe@gmail.com>
2023-08-09 01:52:12 -0700

    Cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,19 @@
 module Main where
 
-import T2B (runT2B, T2BError (InvalidCommand, SyntaxError))
-import System.IO (hPutStrLn, stderr)
+import Control.Monad.Error.Class (MonadError(throwError))
 import System.Exit (exitFailure)
+import System.IO (hPutStrLn, stderr)
+import T2B (runT2B, T2BError (InvalidCommand, SyntaxError))
+import Text.Parsec (runParserT)
 
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.Text as Text
 import qualified T2B.Parser as T2B
-import Text.Parsec (runParser, runParserT)
-import Control.Monad.Error.Class (MonadError(throwError))
 
 main :: IO ()
 main = do
   input <- getContents
   result <- runT2B $ do
-    -- TODO (thosakwe): Parse input here
     parseResult <- runParserT T2B.parser () "stdin" input
     case parseResult of
       Left err -> throwError $ SyntaxError err
@@ -27,6 +26,5 @@ main = do
     Left (InvalidCommand command) -> do
       hPutStrLn stderr $ "Invalid command: " ++ (Text.unpack command)
       exitFailure
-
     Left (SyntaxError parseError) -> do
       hPutStrLn stderr $ "Syntax error: " ++ show parseError
